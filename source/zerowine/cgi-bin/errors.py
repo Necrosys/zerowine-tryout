d7a21248c26b5890b98bfd5ac50aa78dfd8f61a9
Chae Jong Bin <Necrosys@users.noreply.github.com>
2013-03-31 10:29:59 +0000

    [errors.py]  - Updated Wine exceptions
    [libmalware.py]
     - Code refactoring

--- source/zerowine/cgi-bin/errors.py
+++ source/zerowine/cgi-bin/errors.py
@@ -1,11 +1,23 @@
 RUNTIME_ERRORS = {
-	"wine: Unhandled page fault":"Unhandled page fault detected!",
-	"wine: Unhandled exception":"Unhandled exception detected!",
-	"wine: Unimplemented function":"Call to unimplemented function detected!",
-	"err:seh:raise_exception":"SEH(Structured Exception Handling) error detected!",
 	"wineserver crashed":"Wineserver crash detected!",
+	"err:seh:raise_exception":"SEH(Structured Exception Handling) error detected!",
 	"err:seh:setup_exception_record stack overflow":"Stack overflow detected!",
-	"wine: Unhandled privileged instruction":"Unhandled privileged instruction detected!",
-	"wine: Assertion failed":"Assertion fail detected!",
+
+	# dlls/kernel32/except.c
+	"wine: Unhandled division by zero":"Unhandled division by zero detected!",
+	"wine: Unhandled overflow":"Unhandled integer overflow detected!",
+	"wine: Unhandled array bounds":"Unhandled array bounds detected!",
 	"wine: Unhandled illegal instruction":"Unhandled illegal instruction detected!",
+	"wine: Unhandled stack overflow":"Unhandled stack overflow detected!",
+	"wine: Unhandled privileged instruction":"Unhandled privileged instruction detected!",
+	"wine: Unhandled page fault":"Unhandled page fault detected!",
+	"wine: Unhandled alignment":"Unhandled alignment detected!",
+	"wine: Unhandled ^C":"Unhandled Control-C detected!",
+	"wine: Critical section":"Possible deadlock detected!",
+	"wine: Unimplemented function":"Call to unimplemented function detected!",
+	"wine: Assertion failed":"Assertion fail detected!",
+	"wine: Unhandled interrupt":"Unhandled interrupt in vm86 mode detected!",
+	"wine: Unhandled sti in vm86 mode":"Unhandled sti in vm86 mode detected!",
+	"wine: Unhandled PIC return in vm86 mode":"Unhandled PIC return in vm86 mode detected!",
+	"wine: Unhandled exception":"Unhandled exception detected!"
 	}
