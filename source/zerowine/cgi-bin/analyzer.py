a53ebc2a0bb3dd7bec3ac4d2e833267de5efed7f
Chae Jong Bin <Necrosys@users.noreply.github.com>
2013-09-14 21:02:25 +0000

    [analyzer.py]  - Refactored code

--- source/zerowine/cgi-bin/analyzer.py
+++ source/zerowine/cgi-bin/analyzer.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python
 
 import time
-
-from config import *
-from libutils import *
-from libmalware import *
+import os
+import config
+import libutils
+import libmalware
 
 
 def analyze(item, timeout, memory, version, subitem, tags, unpack):
-    printBodyHeader()
+    libutils.printBodyHeader()
 
     startTime = time.ctime()
 
-    fileName, hashes, fileSize = analyzeStatic(item, subitem)
+    fileName, hashes, fileSize = libmalware.analyzeStatic(item, subitem)
 
-    defaultHash = hashes[DEFAULT_HASH_ALGORITHM]
-    dirName = SAMPLE_DIR + os.sep + defaultHash
-    lockName = SAMPLE_DIR + os.sep + LOCK_FILENAME
+    defaultHash = hashes[config.DEFAULT_HASH_ALGORITHM]
+    dirName = config.SAMPLE_DIR + os.sep + defaultHash
+    lockName = config.SAMPLE_DIR + os.sep + config.LOCK_FILENAME
 
-    isRunnable = checkRunnable(timeout, lockName, fileName)
+    isRunnable = libmalware.checkRunnable(timeout, lockName, fileName)
 
     if isRunnable:
-        lockAnalyze(lockName)
+        libmalware.lockAnalyze(lockName)
 
-        msg, execfileName = analyzeDynamic(fileName, timeout, memory, version)
+        msg, execfileName = libmalware.analyzeDynamic(
+            fileName, timeout, memory, version)
 
         idx = -1
         for line in msg:
@@ -39,77 +40,107 @@ def analyze(item, timeout, memory, version, subitem, tags, unpack):
         idx = 0
         execfileName = " "
 
-    headers = getHeaders(fileName)
+    headers = libmalware.getHeaders(fileName)
 
     if unpack == "True":
-        unpackFile(headers, fileName)
+        libmalware.unpackFile(headers, fileName)
 
-    stringList = getStrings(fileName)
+    stringList = libmalware.getStrings(fileName)
     strings = "".join(stringList)
 
-    pdfJavaScript = "".join(getPdfJavaScript(stringList))
-    pdfAnalysis = getPdfAnalysis(headers, fileName)
+    pdfJavaScript = "".join(libmalware.getPdfJavaScript(stringList))
+    pdfAnalysis = libmalware.getPdfAnalysis(headers, fileName)
 
     report = "".join(msg[idx:])
-    signatures = analyzeCalls(msg[idx:])
+    signatures = libmalware.analyzeCalls(msg[idx:])
     if len(signatures) > 0:
         signatures.append("End of signature. See report for more information.")
     signatures = "\n".join(signatures)
 
-    error, warnings = checkRuntimeErrors(msg)
+    error, warnings = libmalware.checkRuntimeErrors(msg)
 
     if error:
-        showWarning("One or more spawned processes crashed while running!")
+        libutils.showWarning(
+            "One or more spawned processes crashed while running!")
 
     if isRunnable:
-        diff = diffFile(dirName)
+        diff = libmalware.diffFile(dirName)
     else:
         diff = " "
 
-    tags = autoTag(tags, diff, "diff")
+    tags = libmalware.autoTag(tags, diff, "diff")
 
     try:
-        diff.append("\nEnd of difference. Use download function for more information.")
+        diff.append(
+            "\nEnd of difference. Use download function for more information.")
     except AttributeError:
         pass
     diff = "".join(diff)
 
-    showDumps(dirName, printHTML=False)
+    libmalware.showDumps(dirName, printHTML=False)
 
-    debuggingtricks = showDebuggingTricks(msg, printHTML=False)
-    vmtricks = showVMDetectionTricks(fileName, dirName, printHTML=False)
+    debuggingtricks = libmalware.showDebuggingTricks(msg, printHTML=False)
+    vmtricks = libmalware.showVMDetectionTricks(
+        fileName, dirName, printHTML=False)
 
     finishTime = time.ctime()
 
     # Save as file
 
-    saveAsFile(item.filename, dirName, FILE_NAME_ORIG_FILENAME)
-    saveAsFile(execfileName, dirName, FILE_NAME_EXEC_FILENAME)
-    saveAsFile("\n".join(hashes.values()), dirName, FILE_HASH_FILENAME)
-    saveAsFile(headers, dirName, FILE_HEADER_FILENAME)
-    saveAsFile(strings, dirName, FILE_STRING_FILENAME)
-    saveAsFile(pdfJavaScript, dirName, FILE_PDF_JAVASCRIPT_ORIG_FILENAME)
-    saveAsFile(pdfAnalysis, dirName, FILE_PDF_ANALYSIS_FILENAME)
-    saveAsFile(str(fileSize), dirName, FILE_SIZE_FILENAME)
+    libmalware.saveAsFile(
+        item.filename,
+        dirName,
+        config.FILE_NAME_ORIG_FILENAME)
+    libmalware.saveAsFile(
+        execfileName,
+        dirName,
+        config.FILE_NAME_EXEC_FILENAME)
+    libmalware.saveAsFile(
+        "\n".join(hashes.values()),
+        dirName,
+        config.FILE_HASH_FILENAME)
+    libmalware.saveAsFile(headers, dirName, config.FILE_HEADER_FILENAME)
+    libmalware.saveAsFile(strings, dirName, config.FILE_STRING_FILENAME)
+    libmalware.saveAsFile(
+        pdfJavaScript,
+        dirName,
+        config.FILE_PDF_JAVASCRIPT_ORIG_FILENAME)
+    libmalware.saveAsFile(
+        pdfAnalysis,
+        dirName,
+        config.FILE_PDF_ANALYSIS_FILENAME)
+    libmalware.saveAsFile(str(fileSize), dirName, config.FILE_SIZE_FILENAME)
 
-    saveAsFile(report, dirName, REPORT_FILENAME)
-    saveAsFile(signatures, dirName, REPORT_SIGNATURE_FILENAME)
-    saveAsFile("\n".join(warnings), dirName, REPORT_WARNING_FILENAME)
+    libmalware.saveAsFile(report, dirName, config.REPORT_FILENAME)
+    libmalware.saveAsFile(
+        signatures,
+        dirName,
+        config.REPORT_SIGNATURE_FILENAME)
+    libmalware.saveAsFile(
+        "\n".join(warnings),
+        dirName,
+        config.REPORT_WARNING_FILENAME)
 
-    saveAsFile(diff, dirName, DIFF_FILENAME)
+    libmalware.saveAsFile(diff, dirName, config.DIFF_FILENAME)
 
-    saveAsFile("\n".join(debuggingtricks), dirName, TRICK_DEBUG_FILENAME)
-    saveAsFile("\n".join(vmtricks), dirName, TRICK_VM_FILENAME)
+    libmalware.saveAsFile(
+        "\n".join(debuggingtricks),
+        dirName,
+        config.TRICK_DEBUG_FILENAME)
+    libmalware.saveAsFile(
+        "\n".join(vmtricks),
+        dirName,
+        config.TRICK_VM_FILENAME)
 
-    saveAsFile(startTime, dirName, ANALYZE_START_FILENAME)
-    saveAsFile(finishTime, dirName, ANALYZE_FINISH_FILENAME)
-    saveAsFile("\n".join(tags), dirName, TAGS_FILENAME)
+    libmalware.saveAsFile(startTime, dirName, config.ANALYZE_START_FILENAME)
+    libmalware.saveAsFile(finishTime, dirName, config.ANALYZE_FINISH_FILENAME)
+    libmalware.saveAsFile("\n".join(tags), dirName, config.TAGS_FILENAME)
 
     if isRunnable:
-        unlockAnalyze(lockName)
+        libmalware.unlockAnalyze(lockName)
 
     # Result link
-    print "<br /><a href='" + CGI_PATH + "/" + CGI_VIEW_FILENAME + "?hash=%s'>View result</a>" % defaultHash
-    print "<br /><br /><a href='" + CGI_PATH + "/" + CGI_DOWNLOAD_FILENAME + "?hash=%s'>Download result</a>" % defaultHash
+    print "<br /><a href='" + config.CGI_PATH + "/" + config.CGI_VIEW_FILENAME + "?hash=%s'>View result</a>" % defaultHash
+    print "<br /><br /><a href='" + config.CGI_PATH + "/" + config.CGI_DOWNLOAD_FILENAME + "?hash=%s'>Download result</a>" % defaultHash
 
-    printBodyFooter()
+    libutils.printBodyFooter()
